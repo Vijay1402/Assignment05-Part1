673793f3eb3049264da6fce64c3af2906dc4b6c3
Vijayaraghavan R <rvrblr@gmail.com>
2023-11-09 14:45:02 +0530

    Assignment

---
+++ Assignment05-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment05_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Vijay";
+            int age = 23;
+            double height = 175.35;
+            Console.WriteLine($"My name is {name}");
+            Console.WriteLine($"My age is {age}");
+            Console.WriteLine($"My height is {height}");
+            Console.ReadKey();
+        }
+    }
+}
